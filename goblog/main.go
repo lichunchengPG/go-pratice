43b7cb31d543f4741d406bec0cc0def88fc400d8
lichuncheng <542432385@qq.com>
2020-12-01 10:29:36 +0800

    goblog:新建博文表单

--- goblog/main.go
+++ goblog/main.go
@@ -8,6 +8,8 @@ import (
 )
 
 
+var router = mux.NewRouter()
+
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		fmt.Fprint(w,"<h1>Hello 欢迎欢迎</h1>")
@@ -42,6 +44,26 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "创建新的文章")
 }
 
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	html := `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 —— 我的技术博客</title>
+</head>
+<body>
+    <form action="%s" method="post">
+        <p><input type="text" name="title"></p>
+        <p><textarea name="body" cols="30" rows="10"></textarea></p>
+        <p><button type="submit">提交</button></p>
+    </form>
+</body>
+</html>
+`
+	storeURL, _ := router.Get("articles.store").URL()
+
+	fmt.Fprintf(w, html, storeURL)
+}
+
 func forceHTMLMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// 1、设置表头
@@ -53,18 +75,21 @@ func forceHTMLMiddleware(h http.Handler) http.Handler {
 
 func removeTrailingSlash(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		// 1. 除首页以外，移除所有请求路径后面的斜杆
+		if r.URL.Path != "/" {
+			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
+		}
 		next.ServeHTTP(w, r)
 	})
 }
 
 func main() {
-	router := mux.NewRouter()
 	router.HandleFunc("/", defaultHandler).Name("home")
 	router.HandleFunc("/about", aboutHandler).Name("about")
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 
 	// 自定义路由页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
