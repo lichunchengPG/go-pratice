d9295dcbeef94d1fb1e84f9bebb855496001a13b
lichuncheng <542432385@qq.com>
2020-12-12 16:59:36 +0800

    保存博文到数据库

--- goblog/main.go
+++ goblog/main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 	"time"
 	"unicode/utf8"
@@ -18,19 +19,20 @@ import (
 
 var router = mux.NewRouter()
 var db *sql.DB
+var test string
 
 func initDB() {
 	var err error
 	config := mysql.Config{
 		User:                 "homestead",
 		Passwd:               "secret",
-		Addr:                 "192.168.10.10",
+		Addr:                 "192.168.10.10:3306",
 		Net:                  "tcp",
 		DBName:               "goblog",
 		AllowNativePasswords: true,
 	}
 	// 准备数据库连接池
-	db, err := sql.Open("mysql", config.FormatDSN())
+	db, err = sql.Open("mysql", config.FormatDSN())
 	checkError(err)
 	// 设置最大连接数
 	db.SetMaxOpenConns(25)
@@ -41,6 +43,19 @@ func initDB() {
 
 	// 尝试连接，失败会报错
 	err = db.Ping()
+	checkError(err)
+
+	createTables()
+}
+
+func createTables() {
+	fmt.Printf("%s", test)
+	createArticlesSQL := `CREATE TABLE IF NOT EXISTS articles(
+    id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    body longtext COLLATE utf8mb4_unicode_ci
+); `
+	_, err := db.Exec(createArticlesSQL)
 	checkError(err)
 }
 
@@ -121,11 +136,14 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
 
 	if len(errors) == 0 {
-		fmt.Fprint(w, "验证通过!<br>")
-		fmt.Fprintf(w, "title 的值为: %v <br>", title)
-		fmt.Fprintf(w, "title 的长度为: %v <br>", utf8.RuneCountInString(title))
-		fmt.Fprintf(w, "body 的值为: %v <br>", body)
-		fmt.Fprintf(w, "body 的长度为: %v <br>", utf8.RuneCountInString(body))
+		lastInsertID, err := saveArticleToDB(title, body)
+		if lastInsertID > 0 {
+			fmt.Fprintf(w, "插入成功，ID 为" + strconv.FormatInt(lastInsertID, 10))
+		} else {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "500 服务器内部错误")
+		}
 	} else {
 
 		storeURL, _ := router.Get("articles.store").URL()
@@ -144,6 +162,36 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w,data)
 	}
 
+}
+
+func saveArticleToDB(title string, body string) (int64, error) {
+	// 初始化变量
+	var (
+		id	int64
+		err	error
+		rs	sql.Result
+		stmt	*sql.Stmt
+	)
+	// 1. 获取一个 prepare 声明语句
+	stmt, err = db.Prepare("INSERT INTO articles (title, body) VALUES(?,?)")
+	// 例行的错误检测
+	if err != nil {
+		return 0, err
+	}
+	// 2. 在此函数运行结束后关闭此语句，防止占用 SQL 连接
+	defer stmt.Close()
+	// 3. 执行请求，传参进入绑定的内容
+	rs, err = stmt.Exec(title,body)
+	if err != nil {
+		return 0, err
+	}
+
+	// 4. 插入成功的话，会返回自增 ID
+	if id, err = rs.LastInsertId(); id > 0 {
+		return id, nil
+	}
+
+	return 0, nil
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
@@ -183,6 +231,7 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 
 func main() {
 	initDB()
+	createTables()
 
 	router.HandleFunc("/", defaultHandler).Name("home")
 	router.HandleFunc("/about", aboutHandler).Name("about")
